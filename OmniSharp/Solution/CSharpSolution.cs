21e490cd9d88f589af881424d16c5485c8eba2fb
Clark.Chambers <Clark.Chambers@dynetics.com>
2018-05-16 09:09:35 -0500

    Added the new project GUID for C#

--- OmniSharp/Solution/CSharpSolution.cs
+++ OmniSharp/Solution/CSharpSolution.cs
@@ -92,6 +92,9 @@ namespace OmniSharp.Solution
                         case "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}": // C# project
                             LoadProject(title, location, guid);
                             break;
+                        case "{9A19103F-16F7-4668-BE54-9A1E7A4F7556}": // C# project
+                            LoadProject(title, location, guid);
+                            break;
                         default:
                                 // Unity3D makes type GUID from the MD5 of title.
                             if (MD5(title) == typeGuid.Substring(1, typeGuid.Length - 2).ToLower().Replace("-", ""))
